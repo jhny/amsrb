d6e17a0b386d74e7484335a941c14c3e2efe198c
Johnny Eradus <jeradus@gmail.com>
2013-02-19 21:07:45 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amsrb::Application.config.secret_token = '1a701ca434a721b80fa653d3e927f07826d40c849609c694e58d2e2bec6532da27411d08515809123f141a419250deab851ec3572953dec43076c72b416d2fa1'
